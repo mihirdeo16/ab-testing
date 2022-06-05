77d9759d1f49d5b3680fb2a427786dcdd2645ef3
mihir-workspace <mihirdeo16@gmail.com>
2022-06-05 15:17:47 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A python library dedicated for A/B testing analysis'
+
+# Setting up
+setup(
+    name="ab_testing",
+    version=VERSION,
+    author="Mihir Deo",
+    author_email="<mihirdeo16@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/mihir-workspace/ab-testing',
+    packages={'','ab_testing'},
+    install_requires=[i.replace('\n','') for i in open('requirements.txt','r').readline()],
+    keywords=['python', 'a/b testing', 'abtest', 'response analysis','conversion rate analysis'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
