6ac276040722b0fa39180bd3586b317d8ce6e4b4
mihir-workspace <mihirdeo16@gmail.com>
2022-08-23 13:10:52 +0530

    Added more topic to increase the reach

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
 VERSION = '0.2.6'
-DESCRIPTION = 'A python library dedicated for A/B testing analysis for experiment testing'
+DESCRIPTION = "A/B testing analysis toolbox for monitoring and reporting experiment results"
 
 # Setting up
 setup(
@@ -18,14 +18,38 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
-    url='https://github.com/mihir-workspace/ab-testing',
+
+    project_urls={
+    'Homepage' : 'https://github.com/mihir-workspace/ab-testing',
+    'Documentation': 'https://github.com/mihir-workspace/ab-testing#readme',
+    'Funding': 'https://donate.pypi.org',
+    'Source': 'https://github.com/mihir-workspace/ab-testing',
+    'Tracker': 'https://github.com/mihir-workspace/ab-testing/issues',
+    },
+
     packages= find_packages(),
     install_requires=["pandas","statsmodels"],
     keywords=['python', 'a/b testing', 'abtest','abtesting', 'response analysis','conversion rate analysis','statistical test'],
+    
     classifiers=[
         "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+
+        "Development Status :: 5 - Production/Stable",
+
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        
+
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Office/Business :: Financial :: Point-Of-Sale",
+
+
     ]
 )
